1d152f3c6b4ecd84942cfb37dd07bf85a194c060
ryf123 <ryf123@gmail.com>
2015-09-14 21:48:19 -0700

    newversion

--- isnumber.py
+++ isnumber.py
@@ -1,38 +1,78 @@
+# Sep 13 9:30--10:00
+# the only letter valid is e
+# it can be e+digit or e-digit or edigit
+# decimal point can't be after e
 class Solution(object):
 	def isNumber(self, s):
 		"""
 		:type s: str
 		:rtype: bool
 		"""
-		s= s.lstrip()
-		s = s.rstrip()
-		num = False
-		dot = False
-		exp = False
-		sign = False
-		s = s.lstrip("+")
-		s = s.lstrip("-")
-		for i,c in enumerate(s):
-			if c.isdigit():
-				num  = True
-			elif c==".":
-				if exp or dot:
+		l = len(s)
+		start,end = 0,l-1
+		while start<end:
+			if s[start] == " ":
+				start+=1
+			else:
+				break
+		while start<end:
+			if s[end] == " ":
+				end -=1
+			else:
+				break
+		if start > end:
+			return False
+		if s[start] == "+" or s[start] == "-":
+			start+=1
+		if start > end:
+			return False
+		e,sign,digit,decimal = False,False,False,False
+		for i in range(start,end+1):
+			if s[i].isdigit():
+				digit = True
+			elif s[i] == "e":
+				if e or sign or not digit:
 					return False
-				dot = True
-			elif c=="e":
-				if exp or not num:
+				if i==end:
 					return False
-				num = False
-				exp = True
-			elif c=="+" or c=="-":
-				if i==0:
+				e = True
+			elif s[i] == "+" or s[i] == "-":
+				if not (i-1 >= start and s[i-1] == "e"):
 					return False
-				if s[i-1] != "e":
+				if i==end:
 					return False
+				sign = True
+			elif s[i] == ".":
+				if e or decimal:
+					return False
+				if i==end and not digit:
+					return False
+				decimal = True
 			else:
 				return False
-		return num
-tests = ["0","2e10","1 a"," 0.1 ","01","0.1e123","123e+123","1e0.1","e9",". 1","-1."]
+		return True
+
+tests = ["0","2e10","1 a"," 0.1 ","01","0.1e123","123e+123","1e0.1","e9",". 1","-1.",".",".1",".+","0e","3.","4e+","e+1"]
 s = Solution()
 for t in tests:
 	print t,s.isNumber(t)
+
+# expected result
+# 0 True
+# 2e10 True
+# 1 a False
+#  0.1  True
+# 01 True
+# 0.1e123 True
+# 123e+123 True
+# 1e0.1 False
+# e9 False
+# . 1 False
+# -1. True
+# . False
+# .1 True
+# .+ False
+# 0e False
+# 3. True
+# 4e+ False
+# e+1 False
--- wordladder.py
+++ wordladder.py
@@ -1,66 +1,55 @@
-class Node():
-	def __init__(self,word,level,prev):
-		self.word1 = word
-		self.level = level
-		self.prev = prev
+import sys
 class Solution(object):
-	def findLadders(self, beginWord, endWord, wordDict):
+	def findLadders(self, start, end, wordlist):
 		"""
-		:type beginWord: str
-		:type endWord: str
-		:type wordDict: Set[str]
-		:rtype: int
+		:type start: str
+		:type end: str
+		:type wordlist: Set[str]
+		:rtype: List[List[int]]
 		"""
 
-		wordDict = list(wordDict)
-		wordDict.append(beginWord)
-		wordDict.append(endWord)
-		if len(wordDict) == 0:
-			return False
-		l = len(wordDict[0])
-		d = {}
-		unvisited = {}
-		stack = [Node(beginWord,1,None)]
-		for word in wordDict:
-			unvisited[word] = True
-		visited = {}
+		wordDict = {}
+		for word in wordlist:
+			wordDict[word] = False
+		wordDict[start] = True
+		mindistance = sys.maxint
+		self.alpha = "abcdefghijklmnopqrstuvwxyz"
+		sols =  self.indict(start,end,wordDict)
 		ret = []
-		minlevel = None
-		prevlvl = 0
-		while (len(stack)):
-			wordnode = stack.pop(0)
-			level= wordnode.level
-			if prevlvl < level:
-				for key in visited:
-					if key in unvisited:
-						del unvisited[key]
-			prevlvl = level
-			for x in xrange(l):
-				for c in xrange(26):
-					c = chr(97+c)
-					word2 = wordnode.word1[:x]+c+wordnode.word1[x+1:]
-					if word2 in unvisited and word2 != wordnode.word1:
-						if minlevel != None:
-							if level+1 > minlevel:
-								continue
-						if word2 == endWord :
-							if minlevel == None:
-								minlevel = level +1
-							r = [word2]
-							p = wordnode
-							while p != None:
-								r.append(p.word1)
-								if p.word1 in visited:
-									del visited[p.word1]
-								p = p.prev
-							ret.append(r[::-1])
-						else:
-							stack.append(Node(word2,level+1,wordnode))
-							# visited[word2] = True
+		for sol in sols:
+			mindistance = min(mindistance,len(sol))
+		for sol in sols:
+			if len(sol) == mindistance:
+				ret.append(sol)
 		return ret
+		return ret
+	def indict(self,start,end,wordDict):		
+		if start == end:
+			return [[end]]
+		ret = []
+		
+		for i,c in enumerate(start):
+			for a in self.alpha:
+				transform = start[:i] + a + start[i+1:]				
+				if transform in wordDict and not wordDict[transform]:
+					wordDict[transform] = True
+					if a != c:
+						word = self.indict(transform,end,wordDict)
+						if word:
+							for w in word:
+								ret.append([start]+w)
+
+					wordDict[transform] = False
+		return ret
+		
+
 s= Solution()
 start = "cet"
 end = "ism"
 d = ["kid","tag","pup","ail","tun","woo","erg","luz","brr","gay","sip","kay","per","val","mes","ohs","now","boa","cet","pal","bar","die","war","hay","eco","pub","lob","rue","fry","lit","rex","jan","cot","bid","ali","pay","col","gum","ger","row","won","dan","rum","fad","tut","sag","yip","sui","ark","has","zip","fez","own","ump","dis","ads","max","jaw","out","btu","ana","gap","cry","led","abe","box","ore","pig","fie","toy","fat","cal","lie","noh","sew","ono","tam","flu","mgm","ply","awe","pry","tit","tie","yet","too","tax","jim","san","pan","map","ski","ova","wed","non","wac","nut","why","bye","lye","oct","old","fin","feb","chi","sap","owl","log","tod","dot","bow","fob","for","joe","ivy","fan","age","fax","hip","jib","mel","hus","sob","ifs","tab","ara","dab","jag","jar","arm","lot","tom","sax","tex","yum","pei","wen","wry","ire","irk","far","mew","wit","doe","gas","rte","ian","pot","ask","wag","hag","amy","nag","ron","soy","gin","don","tug","fay","vic","boo","nam","ave","buy","sop","but","orb","fen","paw","his","sub","bob","yea","oft","inn","rod","yam","pew","web","hod","hun","gyp","wei","wis","rob","gad","pie","mon","dog","bib","rub","ere","dig","era","cat","fox","bee","mod","day","apr","vie","nev","jam","pam","new","aye","ani","and","ibm","yap","can","pyx","tar","kin","fog","hum","pip","cup","dye","lyx","jog","nun","par","wan","fey","bus","oak","bad","ats","set","qom","vat","eat","pus","rev","axe","ion","six","ila","lao","mom","mas","pro","few","opt","poe","art","ash","oar","cap","lop","may","shy","rid","bat","sum","rim","fee","bmw","sky","maj","hue","thy","ava","rap","den","fla","auk","cox","ibo","hey","saw","vim","sec","ltd","you","its","tat","dew","eva","tog","ram","let","see","zit","maw","nix","ate","gig","rep","owe","ind","hog","eve","sam","zoo","any","dow","cod","bed","vet","ham","sis","hex","via","fir","nod","mao","aug","mum","hoe","bah","hal","keg","hew","zed","tow","gog","ass","dem","who","bet","gos","son","ear","spy","kit","boy","due","sen","oaf","mix","hep","fur","ada","bin","nil","mia","ewe","hit","fix","sad","rib","eye","hop","haw","wax","mid","tad","ken","wad","rye","pap","bog","gut","ito","woe","our","ado","sin","mad","ray","hon","roy","dip","hen","iva","lug","asp","hui","yak","bay","poi","yep","bun","try","lad","elm","nat","wyo","gym","dug","toe","dee","wig","sly","rip","geo","cog","pas","zen","odd","nan","lay","pod","fit","hem","joy","bum","rio","yon","dec","leg","put","sue","dim","pet","yaw","nub","bit","bur","sid","sun","oil","red","doc","moe","caw","eel","dix","cub","end","gem","off","yew","hug","pop","tub","sgt","lid","pun","ton","sol","din","yup","jab","pea","bug","gag","mil","jig","hub","low","did","tin","get","gte","sox","lei","mig","fig","lon","use","ban","flo","nov","jut","bag","mir","sty","lap","two","ins","con","ant","net","tux","ode","stu","mug","cad","nap","gun","fop","tot","sow","sal","sic","ted","wot","del","imp","cob","way","ann","tan","mci","job","wet","ism","err","him","all","pad","hah","hie","aim","ike","jed","ego","mac","baa","min","com","ill","was","cab","ago","ina","big","ilk","gal","tap","duh","ola","ran","lab","top","gob","hot","ora","tia","kip","han","met","hut","she","sac","fed","goo","tee","ell","not","act","gil","rut","ala","ape","rig","cid","god","duo","lin","aid","gel","awl","lag","elf","liz","ref","aha","fib","oho","tho","her","nor","ace","adz","fun","ned","coo","win","tao","coy","van","man","pit","guy","foe","hid","mai","sup","jay","hob","mow","jot","are","pol","arc","lax","aft","alb","len","air","pug","pox","vow","got","meg","zoe","amp","ale","bud","gee","pin","dun","pat","ten","mob"]
-print s.findLadders(start,end,d)
+# start = "hit"
+# end = "cog"
+# d = [start,end]+["hot","dot","dog","lot","log"]
+ret = s.findLadders(start,end,d)
+print len(ret),ret
 
