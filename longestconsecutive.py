1d4ef9b16193a231dd6920346c4429cf86f058cd
ryf123 <ryf123@gmail.com>
2015-09-18 22:31:00 -0700

    longest consecutive using dictionary

--- longestconsecutive.py
+++ longestconsecutive.py
@@ -5,37 +5,28 @@ class Solution(object):
 		:rtype: int
 		"""
 		l = len(nums)
-		if l == 0:
-			return 0
-		maxnum,minnum = max(nums),min(nums)
-
-		gap = (maxnum - minnum)/l
-		buckets = [[] for x in xrange(l+1)]
+		d = {}
+		maxlen = 0
 		for num in nums:
-			index = num/max(gap,1)
-			buckets[index].append(num)
-		print buckets
-		newnums = []
-		for bucket in buckets:
-			bucket.sort()
-			newnums += bucket
-		# print newnums
-		for i,num in enumerate(newnums):
-			# print i,num,nums[i-1]
-			if i==0:
-				maxlen,con = 1,1
-			else:
-				if num-newnums[i-1] ==1:
-					con +=1
-					maxlen = max(con,maxlen)
-				else:
-					con =1
+			d[num] =True
+		for key in d:
+			if d[key]:
+				j = 1
+				currentlen = 1
+				while (key+j) in d:
+					currentlen +=1
+					d[key+j] = False
+					j+=1
+					# print j
+				k=1
+				while (key-k) in d:
+					currentlen +=1
+					d[key-k] = False
+					k+=1
+					# print k
+				maxlen = max(currentlen,maxlen)
 		return maxlen
+
+
 s = Solution()
-print s.longestConsecutive([0,-1])
-
-
-
-
-
-
+print s.longestConsecutive([100, 4, 200, 1, 3, 2,0,-1])
