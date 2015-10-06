1646eb39e0f43a8c424d74fb5b0b3e63ebd202ce
ryf123 <ryf123@gmail.com>
2015-10-06 09:14:15 -0700

    add new info

--- questionpicker.py
+++ questionpicker.py
@@ -15,19 +15,32 @@ class QuestionPicker:
 					self.questions.append(m.groups(1)[0])
 	def generateRandom(self):
 		index = int(random.uniform(0,len(self.questions)-1))
-		self.getquestion(self.questions[index])
+		count = 0
+		while not self.getquestion(self.questions[index]):
+			count +=1 
+			if count == 10:
+				print "can't read any question, network issue?"
+				break
 	def getquestion(self,url):
 		url = url.replace(" ","-")
 		print url
-		urlstr = "https://leetcode.com/problems/"+url 
-		url = urlopen(urlstr)
-		lines = ""
-		for line in url:
-			lines +=line
-		pq = PyQuery(lines)
-		tag = pq("meta")
-		desc = tag("meta").eq(2)
-		print desc.attr("content")
+		urlstr = "https://leetcode.com/problems/"+url
+		try: 
+			url = urlopen(urlstr)
+			lines = ""
+			for line in url:
+				lines +=line
+			pq = PyQuery(lines)
+			tag = pq("meta")
+			desc = tag("meta").eq(2)
+			print desc.attr("content")
+			tag = pq("span")
+			difficulty = tag("span.total-submit")
+			print difficulty.text()
+		except Exception,e:
+			print "Question is locked %s,no permission"%(str(url))
+			return False
+		return True
 qp = QuestionPicker()
-print qp.generateRandom()
-qp.getquestion("")
+qp.generateRandom()
+# qp.getquestion("Intersection-of-Two-Linked-Lists")
