e0a6531325689d2b533200747de471f94213baf6
ryf123 <yifeiren@yifeideMacBook-Air.local>
2015-11-17 21:26:48 -0800

    reduce complexity to O(n)

--- removeelement.py
+++ removeelement.py
@@ -5,28 +5,16 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        nums.sort()
         l = len(nums)
-        p1 = 0
-        p2 = l-1
-        counter = 0
-        while(p1<=p2):
-            if val == nums[p1]:
-                temp = nums[p1]
-                nums[p1] = nums[p2]
-                nums[p2] = temp
-                counter +=1
-                if nums[p1] == val:
-                    if p1 != p2:
-                        counter+=1
-                p1+=1
-                p2-=1
-
-            else:
-                p1+=1
-        return l-counter
+        end = l
+        for i in range(0,l):
+            num = nums[l-i-1]
+            if num == val:
+                nums[l-i-1] = nums[end-1]
+                end -=1
+        return end if end > 0 else 0
 s = Solution()
-nums = [5,2,2,1]
+nums = []
 val = 2
 l = s.removeElement(nums,val)
 print nums[:l]
