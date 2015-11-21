2479c2206f6be225a06cebfc7a5a7b2138592507
ryf123 <yifeiren@yifeideMacBook-Air.local>
2015-11-21 13:56:00 -0800

    divide two integers

--- divide.py
+++ divide.py
@@ -8,7 +8,7 @@ class Solution(object):
 		if divisor == 0:
 			return 2147483647
 
-		ret = self.conmpute(abs(dividend),abs(divisor),0)
+		ret = self.conmpute(abs(dividend),abs(divisor))
 
 		if dividend < 0 and divisor > 0:
 			if abs(dividend) % abs(divisor):
@@ -26,36 +26,26 @@ class Solution(object):
 				return 2147483647
 			else:
 				return ret
-	def conmpute(self,dividend,divisor,sol):
-		if sol >= 2147483648:
-			return 2147483648
-		if divisor > dividend:
-			return sol
-		if divisor <= dividend and (divisor << 1) > dividend:
-			return sol +1
-		current = divisor
-		currentcol = 0
-		while(True):
-			# print dividend,current,currentcol
-			current <<=1
-			if current <= dividend:
-				if currentcol ==0:
-					currentcol =2
-				else:
-					currentcol *=2
-			else:
-				current = current >>1
-				dividend = dividend - current
-				# if dividend >= divisor:
-				# 	currentcol +=1
-				# print sol,currentcol,dividend,divisor
-				return self.conmpute(dividend,divisor,sol+currentcol)
+	def conmpute(self,dividend,divisor):
+		sol = 0
+		while dividend >= divisor:
+			shift = 0
+			currentdivisor = divisor
+			while dividend >= currentdivisor<<1:
+				currentdivisor <<=1
+				shift +=1
+			sol += 1<<shift 
+			dividend -= divisor<<shift
+			if sol >= 2147483648:
+				return 2147483648
+		return sol
+
 s = Solution()
-# div = -5
-# for x in range(3,10000):
-# 	if s.divide(x,div) != x/div:
-# 		print x,s.divide(x,div),x/div
-print s.divide(-2147483648,-1)
-print s.divide(-2147483650, 1)
-print s.divide(1026117192, -874002063)
-print s.divide(-1039162657, 490823224)
+div = -3
+for x in range(3,100):
+	if s.divide(x,div) != x/div:
+		print x,s.divide(x,div),x/div
+# print s.divide(-2147483648,-1)
+# print s.divide(-2147483650, 1)
+# print s.divide(1026117192, -874002063)
+# print s.divide(-1039162657, 490823224)
