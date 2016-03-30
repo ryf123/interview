27803dfc43e0f2109692bcdc8f278cbcf1564587
ryf123 <yifeiren@yifeideMacBook-Air.local>
2016-03-29 19:34:13 -0700

    lru cache
    simplify the code by using dummy tail/head

--- lrucache.py
+++ lrucache.py
@@ -9,21 +9,21 @@ class Node:
 
 class LRUCache(object):
 
-
+    head = Node(100,100)
+    end = Node(-1,-1)
     def __init__(self, capacity):
         """
         :type capacity: int
         """
         self.capacity = capacity
         self.d = {}
-        self.head = None
-        self.end = None
+        self.head.prev = self.end
+        self.end.next = self.head
 
     def get(self, key):
         """
         :rtype: int
         """
-        # print "get",key
         if key in self.d:
             n = self.d[key]
             self.remove(n)
@@ -32,29 +32,16 @@ class LRUCache(object):
         else:
             return -1
     def remove(self,n):
-        if n.prev == None and n.next != None:
-            self.end = n.next
-            n.next.prev = None
-        elif n.next == None and n.prev != None:
-            self.head = n.prev
-            n.prev.next = None
-        elif n.prev == None and n.next == None:
-            self.head = None
-            self.end =  None
-        else:
-            n.next.prev = n.prev
-            n.prev.next = n.next
-    def setHead(self,n):
-        # print "set",n.key
-        n.next = None
-        n.prev = self.head
-        if self.head != None:
-            self.head.next = n
+        n.next.prev = n.prev
+        n.prev.next = n.next
 
-        self.head = n
-        if self.end == None:
-            self.end = self.head
-        # print "sethead",self.head.key,self.end.key
+        
+    def setHead(self,n):
+        n.prev = self.head.prev
+        n.prev.next = n
+        self.head.prev = n
+        n.next = self.head
+
     def set(self, key, value):
         """
         :type key: int
@@ -64,23 +51,19 @@ class LRUCache(object):
         if key in self.d:
             old = self.d[key]
             old.val = value
-            # print old.val
             self.remove(old)
             self.setHead(old)
         else:
             created = Node(key,value)
             if len(self.d) < self.capacity:
-                self.setHead(created)
+                pass
             else:
-                del self.d[self.end.key]
-                self.remove(self.end)
-                self.setHead(created)
+                del self.d[self.end.next.key]
+                self.remove(self.end.next)
+            self.setHead(created)
             self.d[key] = created
-        # for key in self.d:
-        #     print key,self.d[key].val
-        # print "#######"
 lruc = LRUCache(2)
-lruc.set(2,1)
+lruc.set(2,2)
 lruc.set(1,1)
 print lruc.get(2)
 lruc.set(4,1)
