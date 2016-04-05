517aa38f2c4f08df6d9f03b0cbb25c63e4e089dc
ryf123 <yifeiren@yifeideMacBook-Air.local>
2016-04-05 09:07:11 -0700

    three way quick sort

--- sortcolors.py
+++ sortcolors.py
@@ -1,18 +1,29 @@
 class Solution:
+    def swap(self,nums,i,j):
+        if i != j:
+            nums[i] = nums[i] ^ nums[j]
+            nums[j] = nums[i] ^ nums[j]
+            nums[i] = nums[i] ^ nums[j]
     # @param {integer[]} nums
     # @return {void} Do not return anything, modify nums in-place instead.
+    # red<white<blue
     def sortColors(self, nums):
-        d = [0]*3
-        for i,num in enumerate(nums):
-            d[num] +=1
-    	i = 0
-    	# print d 
-    	for color in xrange(3):
-	    	while(d[color] > 0):
-	    		nums[i] = color
-	    		d[color] -=1
-	    		i+=1
+        pivot = 1
+        red_p = 0
+        white_p = 0
+        blue_p = len(nums) - 1
+        while white_p <= blue_p:
+            if nums[white_p] == pivot:
+                white_p += 1 
+            elif nums[white_p] < pivot:
+                self.swap(nums,red_p,white_p)
+                red_p += 1
+                white_p += 1
+            else:
+                self.swap(nums,blue_p,white_p)
+                blue_p -= 1
+
 s = Solution()
-nums = [2,2,2]
+nums = [2]
 s.sortColors(nums)
 print nums            
